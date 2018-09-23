86bec27e77a29f93230ec113c3aeeb784075e405
Livingmuseum-02 <livingmuseum-02@Khanittas-iMac.local>
2018-09-23 13:21:04 +0700

    home

--- Test-ProjectM/src/pages/home/home.ts
+++ Test-ProjectM/src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MenuPage } from '../home/home';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  items = [
+    {id: 1, name: "MenuPage", address: "Bankok",},
+  ];
   constructor(public navCtrl: NavController) {
 
   }
+  go2Detail(){
+    this.navCtrl.push(MenuPage);
+  }
 
+  itemSelected(item){//MenuPage
+    console.log("Clicked items;" + item);//Clicked items; HomePage
+    this.navCtrl.push(HomePage, {myItem: item});
+    }
+  
 }
+
