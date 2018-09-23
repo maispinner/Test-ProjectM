b0dcb3af01dc916a07b396303b6372073eb93ebb
Apple <apple@FERNNNN.local>
2018-09-23 13:11:35 +0700

    app.moudule and DescriptPage

---
+++ Test-ProjectM/src/pages/DescriptPage/DescriptPage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-DescriptPage',
+  templateUrl: 'DescriptPage.html'
+})
+export class DescriptPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
